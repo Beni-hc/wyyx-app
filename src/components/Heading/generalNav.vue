d71ce66ad14977591b500cd5f484f5463bae6b04
Beni-hc <z314056860@163.com>
2020-10-25 23:11:10 +0800

    updata

---
+++ src/components/Heading/generalNav.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="general">
+        <div class="generalLeft">
+            <img src="../../assets/house.png" />
+        </div>
+        <div class="generalCenter">值得买</div>
+        <div class="generalRight">
+            <img class="generalimg" src="../../assets/fangdaj.png" alt="" />
+            <img src="../../assets/gouwuche.png" alt="" />
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="stylus" scoped>
+.general
+    height 100px
+    padding 0 24px
+    position relative
+    top 0
+    bottom 0
+    left 0
+    right 0
+    display flex
+    justify-content space-between
+    align-items center
+.generalLeft
+    margin-right 86px
+    img
+        width 50px
+        height 50px
+        padding 4px 4px 0px 0
+.generalCenter
+    font-size 36px
+.generalRight
+    margin-right 3px
+    img
+        width 50px
+        height 50px
+        padding 4px 2px 0px 2px
+.generalimg
+    margin-right 36px
+</style>
--- src/pages/Worth/Worth.vue
+++ src/pages/Worth/Worth.vue
@@ -1,13 +1,62 @@
 <template>
     <div>
-        值得买
+        <heading>
+            <generalNav />
+        </heading>
+        <div class="worthtop">
+            <img src="../../DATA/dataimg/worthimg/beijtu.jpg" alt="" />
+            <img src="../../DATA/dataimg/worthimg/zhidemai.png" alt="" />
+            <span>严选好物 用心生活</span>
+            <div class="bannerBox">111111</div>
+        </div>
+        <div class="lai"></div>
     </div>
 </template>
 <script>
+import heading from "../../components/Heading/Heading";
+import generalNav from "../../components/Heading/generalNav";
 export default {
-    
-}
+    name: "worth",
+    components: {
+        heading,
+        generalNav,
+    },
+};
 </script>
 <style lang="stylus" scoped>
-
+.worthtop
+    width 100%
+    position relative
+    top 0
+    left 0
+    z-index 1
+    padding-top 143px
+    img
+        position absolute
+        &:nth-child(1)
+            width 100%
+            top 0
+            left 0
+        &:nth-child(2)
+            top 60px
+            left 12px
+            width 130px
+    span
+        position absolute
+        top 81px
+        left 146px
+        font-size 30px
+        color #fff
+.bannerBox
+    width 710px
+    height 540px
+    margin 0 auto
+    border-radius 12px
+    background-color red
+    position relative
+    top 0
+.lai
+    height 3000px
+</style>
+<style >
 </style>
